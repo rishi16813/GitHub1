229364a7253755a71a3685dbba52f1439d5c5924
Rishi Singh <rishi16813@gmail.com>
2023-04-10 12:44:40 +0530

    First commit...

---
+++ src/com/first/Test1.java
@@ -0,0 +1,16 @@
+package com.first;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		if (hungry == "yes") {
+			System.out.println("Eat Something");
+		}else {
+			System.out.println("Not Hungry");
+		}
+
+	}
+
+}
